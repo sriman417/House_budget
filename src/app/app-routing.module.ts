3f9c01707d7336860e642975850809521720091f
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-07-18 19:37:15 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SummaryComponent } from './summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponent },
+  { path: 'summary', component: SummaryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
